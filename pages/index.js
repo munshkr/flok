737d0dd13cf8b62030404460ae97facc6998af25
Damián Silvani <munshkr@gmail.com>
2019-08-09 22:49:09 -0300

    Add form for typing session name on index page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,44 @@
+import Router from "next/router";
 import React from "react";
 import FlockScene from "../components/FlockScene";
 import Layout from "../components/Layout";
+
+class NewSessionForm extends React.Component {
+  state = {
+    name: ""
+  };
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    const { name } = this.state;
+    e.preventDefault();
+    Router.push(`/s/${name}`);
+  };
+
+  render() {
+    const { name } = this.state;
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div className="field">
+          <div className="control">
+            <input
+              name="name"
+              onChange={this.handleChange}
+              value={name}
+              className="input is-medium"
+              type="text"
+              placeholder="Type session name and press Enter"
+            />
+          </div>
+        </div>
+      </form>
+    );
+  }
+}
 
 const IndexPage = () => (
   <Layout>
@@ -8,8 +46,12 @@ const IndexPage = () => (
       <div className="container">
         <h1 className="title">flok</h1>
         <h3 className="subtitle">collaborative live coding editor</h3>
+
+        {/* <SessionList /> */}
+        <NewSessionForm />
       </div>
     </section>
+
     <FlockScene />
   </Layout>
 );
