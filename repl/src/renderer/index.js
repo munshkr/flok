e450a496c07bd3b73bf5e172d9763af3da4c6cf2
Damián Silvani <munshkr@gmail.com>
2019-12-30 16:03:43 -0300

    Remove repl/ package - migrated to separate repo

--- repl/src/renderer/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-// import "./style.css";
-import "./styles.scss";
-
-import AppWindow from "../components/AppWindow";
-
-ReactDOM.render(<AppWindow />, document.getElementById("app"));
