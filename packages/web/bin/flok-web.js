7df62aa53e4d4c1616d4eb167d86c83951c6cd1d
Damián Silvani <munshkr@gmail.com>
2020-01-03 18:21:02 -0300

    web: Fix --mongodb parameter

--- packages/web/bin/flok-web.js
+++ packages/web/bin/flok-web.js
@@ -10,7 +10,7 @@ program
   .option("-P, --port [PORT]", "Server port", 3000)
   .option("--secure", "Use secure connection (wss://)", false)
   .option("-d, --dev", "Enable development mode", false)
-  .option("--mongodb-uri URI", "Use MongoDB at URI as document database")
+  .option("--mongodb [URI]", "Use MongoDB at URI as document database")
   .parse(process.argv);
 
 const server = new Server({
@@ -18,7 +18,7 @@ const server = new Server({
   port: program.port,
   isSecure: program.secure,
   isDevelopment: program.dev,
-  mongoDbUri: program.mongodb_uri
+  mongoDbUri: program.mongodb
 });
 
 server.start();
