986353f7af853d0d16bd297fbf07adf889d4a616
Damián Silvani <munshkr@gmail.com>
2020-01-30 21:05:27 -0300

    web: Make TargetMessagesPane scrollable

--- packages/web/components/TargetMessagesPane.js
+++ packages/web/components/TargetMessagesPane.js
@@ -3,15 +3,17 @@ import PropTypes from "prop-types";
 
 const TargetMessagesPane = ({ messages, className }) => (
   <div className={`target-messages-pane ${className}`}>
-    <ol>
-      {messages.map((message, i) => (
-        <li key={i}>
-          <pre className={message.type === "stderr" ? "error" : ""}>
-            {message.body.join("\n").trim()}
-          </pre>
-        </li>
-      ))}
-    </ol>
+    <div className="scrollable-content">
+      <ol>
+        {messages.map((message, i) => (
+          <li key={i}>
+            <pre className={message.type === "stderr" ? "error" : ""}>
+              {message.body.join("\n").trim()}
+            </pre>
+          </li>
+        ))}
+      </ol>
+    </div>
   </div>
 );
 
