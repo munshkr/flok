1bfab1fff4146f47d4bef1de84bc32866fcd535a
Damián Silvani <munshkr@gmail.com>
2020-03-14 15:02:52 -0300

    web: Add text field for setting targets

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -4,9 +4,10 @@ import FlockScene from "../components/FlockScene";
 import Layout from "../components/Layout";
 import uuid from "uuid/v4";
 
-class NewSessionForm extends Component<{}, { user: string }> {
+class NewSessionForm extends Component<{}, { user: string; targets: string }> {
   state = {
-    user: ""
+    user: "",
+    targets: ""
   };
 
   handleChangeUser = (e: ChangeEvent) => {
@@ -14,15 +15,45 @@ class NewSessionForm extends Component<{}, { user: string }> {
     this.setState({ user: target.value });
   };
 
+  handleChangeTargets = (e: ChangeEvent) => {
+    const target = e.target as HTMLInputElement;
+    this.setState({
+      targets: target.value
+    });
+  };
+
   handleSubmit = (e: FormEvent) => {
     e.preventDefault();
     const session = btoa(uuid());
-    Router.push(`/s/${session}`);
+    const { targets } = this.state;
+    const layout = targets
+      .split(",")
+      .map(s => s.trim())
+      .join(",");
+    Router.push(`/s/${session}?layout=${layout}`);
   };
 
   render() {
+    const { targets } = this.state;
+
     return (
       <form onSubmit={this.handleSubmit}>
+        <div className="field">
+          <div className="control">
+            <input
+              name="user"
+              onChange={this.handleChangeTargets}
+              value={targets}
+              className="input is-large"
+              type="text"
+              placeholder={
+                "Enter the list of targets the session will use, separated by commas (e.g. tidal,foxdot,hydra)"
+              }
+              autoFocus
+            />
+          </div>
+        </div>
+
         <div className="field">
           <div className="control">
             <button type="submit" className="button is-link is-large">
