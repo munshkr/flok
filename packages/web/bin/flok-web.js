8567b43eb63bf97fc987202642f50a7b8ca8657e
Damián Silvani <munshkr@gmail.com>
2020-05-09 16:51:11 -0300

    Remove --redirect-https option
    This option was only intended as a solution for the Heroku server, to
    automatically redirect from http to https.

--- packages/web/bin/flok-web.js
+++ packages/web/bin/flok-web.js
@@ -9,18 +9,12 @@ program
   .option("-H, --host [HOST]", "Server host", "0.0.0.0")
   .option("-P, --port [PORT]", "Server port", 3000)
   .option("-s, --secure", "Serve on https (use SSL)", false)
-  .option(
-    "--redirect-https",
-    "Automatically redirect from http to https",
-    false
-  )
   .parse(process.argv);
 
 const server = new Server({
   host: program.host,
   port: program.port,
   secure: program.secure,
-  redirectHttps: program.redirectHttps
 });
 
 server.start();
