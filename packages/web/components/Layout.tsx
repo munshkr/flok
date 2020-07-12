ed01b153453fe2de55b85ef203fa7439e4d1f513
Damián Silvani <munshkr@gmail.com>
2020-07-11 22:26:50 -0300

    Move remote-caret styles to a separate stylesheet

--- packages/web/components/Layout.tsx
+++ packages/web/components/Layout.tsx
@@ -23,38 +23,11 @@ export default ({ children }: { children: any }) => (
         width: 10px;
         height: 10px;
       }
-
       ::-webkit-scrollbar-thumb {
         background: rgba(90, 90, 90, 0.4);
       }
-
       ::-webkit-scrollbar-track {
         background: rgba(0, 0, 0, 0.2);
-      }
-
-      .remote-caret {
-        position: absolute;
-        border-left: black;
-        border-left-style: solid;
-        border-left-width: 2px;
-        height: 1.1em;
-      }
-
-      .remote-caret > div {
-        position: relative;
-        top: 1.5em;
-        left: -2px;
-        font-size: 14px;
-        background-color: #fa8100;
-        font-family: Monaco, monospace;
-        font-style: normal;
-        font-weight: normal;
-        line-height: normal;
-        user-select: none;
-        color: white;
-        padding-left: 2px;
-        padding-right: 2px;
-        z-index: 3;
       }
 
       @media only screen and (max-width: 600px) {
