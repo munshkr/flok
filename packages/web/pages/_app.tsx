e80e293aa259be6bb35e0c69760bbe0e31e928c9
Damián Silvani <munshkr@gmail.com>
2020-07-11 22:26:50 -0300

    Move remote-caret styles to a separate stylesheet

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -6,6 +6,8 @@ import "codemirror/lib/codemirror.css";
 import "codemirror/theme/material.css";
 import "codemirror/addon/scroll/simplescrollbars.css";
 
+import "../lib/y-codemirror.css";
+
 export default function CustomApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
