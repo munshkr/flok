83ed2071558417977c241987fdff414efbc08a5f
Damián Silvani <munshkr@gmail.com>
2020-08-16 09:55:06 -0300

    Import fonts

---
+++ packages/web/styles/theme.js
@@ -0,0 +1,14 @@
+const Theme = {
+  fontFamily: {
+    sansSerif: "Chivo, sans-serif",
+    mono: "'Roboto Mono', monospace"
+  },
+  colors: {
+    foreground: "#efefef",
+    background: "#101010",
+    link: "#1eaaf1",
+    linkHover: "#0d8ecf"
+  }
+};
+
+export default Theme;
