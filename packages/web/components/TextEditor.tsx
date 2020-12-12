5927481f330427a23ed2e5c9f1c3248faedb13d9
Damián Silvani <munshkr@gmail.com>
2020-12-12 20:26:36 -0300

    web: Reformat TextEditor

--- packages/web/components/TextEditor.tsx
+++ packages/web/components/TextEditor.tsx
@@ -182,7 +182,7 @@ class TextEditor extends Component<Props, {}> {
 
     this.evaluate(content, 0, content.length, locally);
   };
-  
+
   evaluate(body: string, fromLine: number = -1, toLine: number = -1, locally: boolean = false) {
     const { editorId, target, onEvaluateCode, sessionClient } = this.props;
 
@@ -232,20 +232,22 @@ class TextEditor extends Component<Props, {}> {
       "Ctrl-Alt-Enter": () => this.evaluateBlock(true),
       "Cmd-Alt-Enter": () => this.evaluateBlock(true),
     };
-    
-    // Repalce shortkeys when using Mercury
+
+    // Replace shortkeys when using Mercury
     // Because Mercury always replaces the entire code with the newly
     // executed page. No per-line evaluation
-    if (target === 'mercury'){
-      defaultExtraKeys = { ...{
-        "Shift-Enter": () => this.evaluateAll(false),
-        "Ctrl-Enter": () => this.evaluateAll(false),
-        "Cmd-Enter": () => this.evaluateAll(false),
-        "Alt-Enter": () => this.evaluateAll(false),
-        "Shift-Alt-Enter": () => this.evaluateAll(true),
-        "Ctrl-Alt-Enter": () => this.evaluateAll(true),
-        "Cmd-Alt-Enter": () => this.evaluateAll(true),
-      } };
+    if (target === 'mercury') {
+      defaultExtraKeys = {
+        ...{
+          "Shift-Enter": () => this.evaluateAll(false),
+          "Ctrl-Enter": () => this.evaluateAll(false),
+          "Cmd-Enter": () => this.evaluateAll(false),
+          "Alt-Enter": () => this.evaluateAll(false),
+          "Shift-Alt-Enter": () => this.evaluateAll(true),
+          "Ctrl-Alt-Enter": () => this.evaluateAll(true),
+          "Cmd-Alt-Enter": () => this.evaluateAll(true),
+        }
+      };
     }
 
     const extraKeys = {
