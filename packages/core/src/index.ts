727cefdacde7d22be014bf2fddaba549432a5b1d
Damián Silvani <munshkr@gmail.com>
2022-06-17 11:03:35 -0300

    feat(core): Define list of non-block eval targets

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -5,5 +5,6 @@ import Subscription from './Subscription';
 const replTargets = ['tidal', 'sclang', 'remote_sclang', 'foxdot', 'mercury'];
 const webTargets = ['hydra', 'strudel'];
 const allTargets = [...replTargets, ...webTargets];
+const nonBlockEvalTargets = ['mercury', 'strudel'];
 
-export { PubSub, PubSubClient, Subscription, replTargets, webTargets, allTargets };
+export { PubSub, PubSubClient, Subscription, replTargets, webTargets, allTargets, nonBlockEvalTargets };
