2d0f500a33465e583e9db02d30ec5161261db326
Damián Silvani <munshkr@gmail.com>
2022-06-16 22:19:04 -0300

    feat(core): Add `strudel` web target

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,18 +2,8 @@ import PubSub from './PubSub';
 import PubSubClient from './PubSubClient';
 import Subscription from './Subscription';
 
-const replTargets = [
-  'tidal',
-  'sclang',
-  'remote_sclang',
-  'foxdot',
-  'mercury'
-]
-
-const webTargets = [
-  'hydra'
-]
-
-const allTargets = [...replTargets, ...webTargets]
+const replTargets = ['tidal', 'sclang', 'remote_sclang', 'foxdot', 'mercury'];
+const webTargets = ['hydra', 'strudel'];
+const allTargets = [...replTargets, ...webTargets];
 
 export { PubSub, PubSubClient, Subscription, replTargets, webTargets, allTargets };
