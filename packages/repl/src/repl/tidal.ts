5d8eda273e8b32737c311aef4201ca957c600653
Damián Silvani <munshkr@gmail.com>
2022-08-22 08:54:45 -0300

    fix(repl): Simplify Tidal repl for newer versions to use `tidal` command script instead of `ghci`
    Also delete bundled `BootTidal.hs` script from Tidal 1.6.1

--- packages/repl/src/repl/tidal.ts
+++ packages/repl/src/repl/tidal.ts
@@ -1,14 +1,13 @@
-import { execSync } from 'child_process';
-import * as path from 'path';
 import { CommandREPL, CommandREPLContext } from '../repl';
-import { readPackageMetadata } from '../index';
 
 class TidalREPL extends CommandREPL {
   constructor(ctx: CommandREPLContext) {
     super(ctx);
 
-    this.command = this.commandPath('ghci');
-    this.args = ['-ghci-script', this.bootScript()];
+    const { bootScript } = this.extraOptions;
+
+    this.command = this.commandPath('tidal');
+    this.args = bootScript ? ['-ghci-script', bootScript] : [];
   }
 
   prepare(body: string): string {
@@ -19,47 +18,6 @@ class TidalREPL extends CommandREPL {
 
   handleData(type: string, lines: string[]): string[] {
     return type == 'stdout' ? lines.map(line => line.replace(/(tidal> )+/i, '')) : lines;
-  }
-
-  bootScript(): string {
-    const { bootScript } = this.extraOptions;
-    return bootScript || this.defaultBootScript();
-  }
-
-  defaultBootScript(): string {
-    return path.join(this.dataDir(), 'BootTidal.hs');
-  }
-
-  dataDir(): string {
-    const ghcPkgCmd = this.commandPath('ghc-pkg');
-    try {
-      const dataDir = execSync(`${ghcPkgCmd} field tidal data-dir`)
-        .toString()
-        .trim();
-      const firstLine = dataDir.split('\n')[0];
-      return firstLine.substring(firstLine.indexOf(' ') + 1);
-    } catch (err) {
-      console.warn(`Failed to get tidal data-dir`);
-
-      const metadata = readPackageMetadata();
-      const tidalMetadata = metadata['tidal'];
-      if (tidalMetadata) {
-        const { version } = tidalMetadata;
-        console.warn(`Going to fallback to embedded BootTidal.hs, for TidalCycles ${version}`);
-        console.warn(
-          `If you have a different TidalCycles version installed, you may want to ` +
-            `specify the location of your TidalCycles bootloading script.\n` +
-            `Read more: https://github.com/munshkr/flok/wiki/Failed-to-get-tidal-data-dir`,
-        );
-        return path.resolve(__dirname, path.join('..', '..', 'data'));
-      } else {
-        console.warn(
-          `You will need to specify the location of your TidalCycles bootloading script.\n` +
-            `Read more: https://github.com/munshkr/flok/wiki/Failed-to-get-tidal-data-dir`,
-        );
-        throw err;
-      }
-    }
   }
 
   commandPath(cmd: string): string {
