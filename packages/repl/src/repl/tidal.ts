7f86a349a63bc11cfcedbbd0705d00a125f60099
Damián Silvani <munshkr@gmail.com>
2022-08-25 20:26:45 -0300

    fix(repl): Revert last changes in TidalRepl, but correctly get data dir now (#151)

--- packages/repl/src/repl/tidal.ts
+++ packages/repl/src/repl/tidal.ts
@@ -1,13 +1,13 @@
+import { execSync } from 'child_process';
+import * as path from 'path';
 import { CommandREPL, CommandREPLContext } from '../repl';
 
 class TidalREPL extends CommandREPL {
   constructor(ctx: CommandREPLContext) {
     super(ctx);
 
-    const { bootScript } = this.extraOptions;
-
-    this.command = this.commandPath('tidal');
-    this.args = bootScript ? ['-ghci-script', bootScript] : [];
+    this.command = this.commandPath('ghci');
+    this.args = ['-ghci-script', this.bootScript()];
   }
 
   prepare(body: string): string {
@@ -18,6 +18,34 @@ class TidalREPL extends CommandREPL {
 
   handleData(type: string, lines: string[]): string[] {
     return type == 'stdout' ? lines.map(line => line.replace(/(tidal> )+/i, '')) : lines;
+  }
+
+  bootScript(): string {
+    const { bootScript } = this.extraOptions;
+    return bootScript || this.defaultBootScript();
+  }
+
+  defaultBootScript(): string {
+    return path.join(this.dataDir(), 'BootTidal.hs');
+  }
+
+  dataDir(): string {
+    const ghciCmd = this.commandPath('ghci');
+    try {
+      const dataDir = execSync(`${ghciCmd} -e Paths_tidal.getDataDir`)
+        .toString()
+        .trim()
+        .replace(/"/g, '');
+      console.log("Data dir:", dataDir);
+      return dataDir;
+    } catch (err) {
+      console.warn(`Failed to get tidal data dir`);
+      console.warn(
+        `You will need to specify the location of your TidalCycles bootloading script.\n` +
+          `Read more: https://github.com/munshkr/flok/wiki/Failed-to-get-tidal-data-dir`,
+      );
+      throw err;
+    }
   }
 
   commandPath(cmd: string): string {
