56c07d09173295f93315615f2bd4c71046abd04c
Cameron Alexander <emptyflash@gmail.com>
2023-01-23 21:17:17 -0500

    Upgrade strudel and use new interface

--- packages/web/lib/StrudelWrapper.ts
+++ packages/web/lib/StrudelWrapper.ts
@@ -1,15 +1,18 @@
-import { evaluate, evalScope } from "@strudel.cycles/eval";
-import { Scheduler } from "@strudel.cycles/webaudio/scheduler.mjs";
-import controls from "@strudel.cycles/core/controls.mjs";
+import { repl, controls, evalScope } from "@strudel.cycles/core";
+import {
+  getAudioContext,
+  initAudioOnFirstClick,
+  webaudioOutput,
+  samples,
+} from '@strudel.cycles/webaudio';
+import { transpiler } from '@strudel.cycles/transpiler';
 
 export type ErrorHandler = (error: string) => void;
 
 class StrudelWrapper {
   initialized: boolean;
   onError: ErrorHandler;
-  _scheduler: any;
-  _getAudioContext: () => AudioContext;
-  _loadWebDirt: (opts: any) => Promise<void>;
+  _repl: any;
 
   constructor(onError: ErrorHandler) {
     this.initialized = false;
@@ -17,14 +20,7 @@ class StrudelWrapper {
   }
 
   async importModules() {
-    const { getAudioContext } = await import(
-      "@strudel.cycles/webaudio/webaudio.mjs"
-    );
-    const { loadWebDirt } = await import("@strudel.cycles/webdirt");
-
-    this._getAudioContext = getAudioContext;
-    this._loadWebDirt = loadWebDirt;
-
+    initAudioOnFirstClick();
     // import desired modules and add them to the eval scope
     await evalScope(
       import("@strudel.cycles/core"),
@@ -38,40 +34,19 @@ class StrudelWrapper {
       import("@strudel.cycles/webdirt"),
       import("@strudel.cycles/xen"),
       controls
-      // import other strudel packages here
-    ); // add strudel to eval scope
+    );
+    await samples('https://strudel.tidalcycles.org/EmuSP12.json', 'https://strudel.tidalcycles.org/EmuSP12/')
   }
 
   async initialize() {
-    if (!this._getAudioContext) {
-      throw new Error(
-        "AudioContext not available. Call importModules() first."
-      );
-    }
-
-    const audioContext = this._getAudioContext();
-    const latency = 0.2;
-
-    // load default samples + init webdirt
-    this._loadWebDirt({
-      audioContext,
-      latency,
-      sampleMapUrl: "https://strudel.tidalcycles.org/EmuSP12.json",
-      sampleFolder: "https://strudel.tidalcycles.org/EmuSP12/",
-    });
-
-    // the scheduler will query the pattern within the given interval
-    this._scheduler = new Scheduler({
-      audioContext,
-      interval: 0.1,
-      latency,
-      onEvent: (hap) => {
-        if (!hap.context.onTrigger) {
-          this.onError(
-            "No output chosen. Use one of: .out(), .webdirt(), .osc()"
-          );
-        }
-      },
+    this._repl = repl({
+      defaultOutput: webaudioOutput,
+      afterEval: ()=>{},
+      beforeEval: ()=>{},
+      onSchedulerError: e=>this.onError(e.toString()),
+      onEvalError: e=>this.onError(e.toString()),
+      getTime: () => getAudioContext().currentTime,
+      transpiler,
     });
 
     this.initialized = true;
@@ -82,9 +57,7 @@ class StrudelWrapper {
     if (!this.initialized) await this.initialize();
 
     try {
-      const { pattern } = await evaluate(code);
-      this._scheduler.setPattern(pattern);
-      this._scheduler.start();
+      await this._repl.evaluate(code);
       this.onError(null);
     } catch (e) {
       console.error(e);
