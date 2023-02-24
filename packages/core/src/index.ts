5ec8431737c61c144091e29dffa5e9e737a685ea
Raphael Forment <raphael.forment@gmail.com>
2023-02-24 20:14:19 +0100

    Adding new proto sardine target

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,7 +2,7 @@ import PubSub from './PubSub';
 import PubSubClient from './PubSubClient';
 import Subscription from './Subscription';
 
-const replTargets = ['tidal', 'sclang', 'remote_sclang', 'foxdot', 'mercury'];
+const replTargets = ['tidal', 'sclang', 'remote_sclang', 'foxdot', 'mercury', 'sardine'];
 const webTargets = ['hydra', 'strudel'];
 const allTargets = [...replTargets, ...webTargets];
 const nonBlockEvalTargets = ['mercury', 'strudel'];
