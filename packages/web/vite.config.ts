0e984e30cf4cf6faf6aa37d5798741cb42d700cb
Damián Silvani <munshkr@gmail.com>
2023-03-08 14:03:59 -0300

    fix(web): Configure manual chunking for builds

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import { defineConfig } from "vite";
+import { splitVendorChunkPlugin } from "vite";
+import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
-})
+  plugins: [preact(), splitVendorChunkPlugin()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          codemirror: [
+            "codemirror",
+            "@codemirror/commands",
+            "@codemirror/lang-javascript",
+            "@codemirror/state",
+            "@codemirror/theme-one-dark",
+            "@codemirror/view",
+          ],
+          yjs: [
+            "yjs",
+            "y-codemirror.next",
+            "y-indexeddb",
+            "y-webrtc",
+            "y-websocket",
+          ],
+        },
+      },
+    },
+  },
+});
