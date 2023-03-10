375d9cf214c6c31231946eb8fa64b4c9c1498c1c
Damián Silvani <munshkr@gmail.com>
2023-03-10 08:59:04 -0300

    refactor(web): Some adjustments on server functions

--- packages/web/index.js
+++ packages/web/index.js
@@ -1,7 +1,7 @@
-import startServer from "./lib/server.js";
+import { startServer } from "./lib/server.js";
 
 startServer({
   hostname: "localhost",
   port: 3000,
-  isDevelopment: process.env.NODE_ENV !== "production"
+  dev: process.env.NODE_ENV !== "production"
 })
--- packages/web/lib/server.js
+++ packages/web/lib/server.js
@@ -1,41 +1,76 @@
-import { createServer } from "http";
+import http from "http";
 import { parse } from "url";
 import next from "next";
 import connect from "connect";
 import withFlokServer from "@flok/server";
+import { networkInterfaces } from "os";
 
-export default function startServer({ hostname, port, isDevelopment }) {
+export async function createServer({ hostname, port, dev, secure, staticDir }) {
   // when using middleware `hostname` and `port` must be provided below
-  const nextApp = next({ dev: isDevelopment, hostname, port });
+  const nextApp = next({ dev, hostname, port });
   const handle = nextApp.getRequestHandler();
 
   console.log("> Preparing app...");
-  nextApp.prepare().then(() => {
-    const app = withFlokServer(connect());
+  await nextApp.prepare()
 
-    app.use(async (req, res) => {
-      try {
-        // Be sure to pass `true` as the second argument to `url.parse`.
-        // This tells it to parse the query portion of the URL.
-        const parsedUrl = parse(req.url, true);
-        await handle(req, res, parsedUrl);
-      } catch (err) {
-        console.error("Error occurred handling", req.url, err);
-        res.statusCode = 500;
-        res.end("internal server error");
-      }
-    });
+  const app = withFlokServer(connect());
 
-    createServer(app)
-      .once("error", (err) => {
-        console.error(err);
-        process.exit(1);
-      })
-      .listen(port, () => {
-        console.log(`> Ready on http://${hostname}:${port}`);
-      });
+  app.use(async (req, res) => {
+    try {
+      // Be sure to pass `true` as the second argument to `url.parse`.
+      // This tells it to parse the query portion of the URL.
+      const parsedUrl = parse(req.url, true);
+      await handle(req, res, parsedUrl);
+    } catch (err) {
+      console.error("Error occurred handling", req.url, err);
+      res.statusCode = 500;
+      res.end("Internal server error");
+    }
   });
+
+  return http.createServer(app)
 }
 
+export async function startServer({ onReady, ...opts }) {
+  try {
+    const scheme = opts.secure ? "https" : "http";
+    const server = await createServer(opts);
 
+    server.listen(opts.port, onReady || (() => {
+      console.log(`> Visit ${scheme}://${opts.hostname || "localhost"}:${opts.port}`);
 
+      const netResults = getPossibleIpAddresses();
+      if (netResults.length > 1) {
+        console.log("> If on LAN, try sharing with your friends one of these URLs:");
+        Object.entries(netResults).map(([k, v]) => {
+          console.log(`\t${k}: ${scheme}://${v}:${opts.port}`);
+        });
+      } else {
+        console.log(
+          `> If on LAN, try sharing with your friends ${scheme}://${Object.values(netResults)[0]}:${opts.port}`);
+      }
+    }))
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+const getPossibleIpAddresses = () => {
+  const nets = networkInterfaces();
+  const results = Object.create(null); // Or just '{}', an empty object
+
+  for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+      // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+      if (net.family === "IPv4" && !net.internal) {
+        if (!results[name]) {
+          results[name] = [];
+        }
+        results[name].push(net.address);
+      }
+    }
+  }
+
+  return results;
+};
