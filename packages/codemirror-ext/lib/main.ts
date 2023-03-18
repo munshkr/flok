5674f8d1918fdc725348293f33c3d3572aa8fa5b
Damián Silvani <munshkr@gmail.com>
2023-03-18 14:00:31 -0300

    feat(codemirror-ext): FlokSession class to encapsulate Yjs and CM6 handling

---
+++ packages/codemirror-ext/lib/main.ts
@@ -0,0 +1,143 @@
+import * as Y from "yjs";
+import { yCollab, yUndoManagerKeymap } from "y-codemirror.next";
+import { IndexeddbPersistence } from "y-indexeddb";
+import { WebrtcProvider } from "y-webrtc";
+import { WebsocketProvider } from "y-websocket";
+import { Awareness } from "y-protocols/awareness.js";
+import { keymap } from "@codemirror/view";
+import * as random from "lib0/random";
+
+export type UserColor = {
+  color: string;
+  light: string;
+};
+
+export const userColors: UserColor[] = [
+  { color: "#30bced", light: "#30bced33" },
+  { color: "#6eeb83", light: "#6eeb8333" },
+  { color: "#ffbc42", light: "#ffbc4233" },
+  { color: "#ecd444", light: "#ecd44433" },
+  { color: "#ee6352", light: "#ee635233" },
+  { color: "#9ac2c9", light: "#9ac2c933" },
+  { color: "#8acb88", light: "#8acb8833" },
+  { color: "#1be7ff", light: "#1be7ff33" },
+];
+
+export type FlokSessionOptions = {
+  hostname?: string;
+  port?: number;
+  isSecure?: boolean;
+  room?: string;
+  user?: string;
+};
+
+export class FlokSession {
+  hostname: string;
+  port: number;
+  isSecure: boolean;
+  room: string;
+
+  yDoc!: Y.Doc;
+  awareness!: Awareness;
+
+  _user: string;
+  _userColor: UserColor;
+
+  _idbProvider!: IndexeddbPersistence;
+  _webrtcProvider!: WebrtcProvider;
+  _wsProvider!: WebsocketProvider;
+
+  constructor(opts: FlokSessionOptions = {}) {
+    this.hostname = opts?.hostname || "localhost";
+    this.port = opts?.port || 3000;
+    this.isSecure = opts?.isSecure || false;
+    this.room = opts?.room || "default";
+
+    this._user = opts?.user || "Anonymous " + Math.floor(Math.random() * 100);
+    this._userColor = userColors[random.uint32() % userColors.length];
+
+    this._prepareYjs();
+  }
+
+  get user(): string {
+    return this._user;
+  }
+
+  get userColor(): UserColor {
+    return this._userColor;
+  }
+
+  set user(newUser: string) {
+    this._user = newUser;
+    this._updateUserStateField();
+  }
+
+  set userColor(newUserColor: UserColor) {
+    this._userColor = newUserColor;
+    this._updateUserStateField();
+  }
+
+  getText(id: string): Y.Text {
+    return this.yDoc.getText(id);
+  }
+
+  getTextString(id: string) {
+    return this.getText(id).toString();
+  }
+
+  _prepareYjs() {
+    this._createDoc();
+    this._createProviders();
+  }
+
+  _createDoc() {
+    this.yDoc = new Y.Doc();
+
+    // Awareness
+    this.awareness = new Awareness(this.yDoc);
+    this._updateUserStateField();
+  }
+
+  _createProviders() {
+    this._idbProvider = new IndexeddbPersistence(this.room, this.yDoc);
+    // this._idbProvider.on("synced", () => {
+    //   console.log("Data from IndexexDB loaded");
+    // });
+
+    this._webrtcProvider = new WebrtcProvider(this.room, this.yDoc, {
+      awareness: this.awareness,
+      signaling: [`${this._wsUrl}/signal`],
+    });
+
+    this._wsProvider = new WebsocketProvider(
+      `${this._wsUrl}/doc`,
+      this.room,
+      this.yDoc,
+      { awareness: this.awareness }
+    );
+
+    // this._wsProvider.on("status", (event: any) => {
+    //   console.log(event.status);
+    // });
+  }
+
+  _updateUserStateField() {
+    this.awareness.setLocalStateField("user", {
+      name: this.user,
+      color: this._userColor.color,
+      colorLight: this._userColor.light,
+    });
+  }
+
+  get _wsUrl() {
+    const schema = this.isSecure ? `wss` : `ws`;
+    return `${schema}://${this.hostname}:${this.port}`;
+  }
+}
+
+export const flokCollabSetup = (session: FlokSession, textId: string) => {
+  return [
+    keymap.of([...yUndoManagerKeymap]),
+    yCollab(session.yDoc.getText(textId), session.awareness),
+  ];
+};
--- packages/codemirror-ext/src/main.ts
+++ packages/codemirror-ext/src/main.ts
@@ -1,73 +1,26 @@
-import * as Y from "yjs";
-import { yCollab, yUndoManagerKeymap } from "y-codemirror.next";
-import { IndexeddbPersistence } from "y-indexeddb";
-import { WebrtcProvider } from "y-webrtc";
-import { WebsocketProvider } from "y-websocket";
-import * as awarenessProtocol from "y-protocols/awareness.js";
-
 import { EditorView, basicSetup } from "codemirror";
-import { keymap } from "@codemirror/view";
 import { javascript } from "@codemirror/lang-javascript";
 import { oneDark } from "@codemirror/theme-one-dark";
-
-import * as random from "lib0/random";
 import { EditorState } from "@codemirror/state";
+
+import { flokCollabSetup, FlokSession } from "../lib/main.js";
 
 import "./style.css";
 
-const usercolors = [
-  { color: "#30bced", light: "#30bced33" },
-  { color: "#6eeb83", light: "#6eeb8333" },
-  { color: "#ffbc42", light: "#ffbc4233" },
-  { color: "#ecd444", light: "#ecd44433" },
-  { color: "#ee6352", light: "#ee635233" },
-  { color: "#9ac2c9", light: "#9ac2c933" },
-  { color: "#8acb88", light: "#8acb8833" },
-  { color: "#1be7ff", light: "#1be7ff33" },
-];
-
-const userColor = usercolors[random.uint32() % usercolors.length];
-
-const ydoc = new Y.Doc();
-
-const awareness = new awarenessProtocol.Awareness(ydoc);
-awareness.setLocalStateField("user", {
-  name: "Anonymous " + Math.floor(Math.random() * 100),
-  color: userColor.color,
-  colorLight: userColor.light,
+const session = new FlokSession({
+  onWsStatus: (event) => {
+    console.log(event.status);
+  },
 });
-
-const idbProvider = new IndexeddbPersistence("flok-room", ydoc);
-idbProvider.on("synced", () => {
-  console.log("Data from IndexexDB loaded");
-});
-
-const webrtcProvider = new WebrtcProvider("flok-room", ydoc, {
-  awareness,
-  signaling: ["ws://localhost:3000/signal"],
-});
-
-const wsProvider = new WebsocketProvider(
-  "ws://localhost:3000/doc",
-  "flok-room",
-  ydoc,
-  { awareness }
-);
-
-wsProvider.on("status", (event: any) => {
-  console.log(event.status); // logs "connected" or "disconnected"
-});
-
-const ytext = ydoc.getText("codemirror");
+const id = "main";
 
 const state = EditorState.create({
-  doc: ytext.toString(),
+  doc: session.getTextString(id),
   extensions: [
-    keymap.of([...yUndoManagerKeymap]),
     basicSetup,
+    flokCollabSetup(session, id),
     javascript(),
     EditorView.lineWrapping,
-    yCollab(ytext, awareness),
     oneDark,
   ],
 });
