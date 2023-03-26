2b50cb476b5396a05fab7c10c25f3a1352d196c4
Damián Silvani <munshkr@gmail.com>
2023-03-26 18:36:53 -0300

    fix(web): Session working - more or less
    yjs is being loaded twice, but that can be solved with a context hook...

--- packages/web/components/editor.tsx
+++ packages/web/components/editor.tsx
@@ -6,6 +6,11 @@ import CodeMirror, { ReactCodeMirrorProps } from "@uiw/react-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
 import { useTheme } from "next-themes";
 import { fontMono } from "@/app/theme";
+import { flashField, evalKeymap } from "@flok/codemirror-eval";
+import { yCollab } from "y-codemirror.next";
+import { UndoManager } from "yjs";
+import { Prec } from "@codemirror/state";
+import type { Session } from "@flok/session";
 
 type CodeMirrorThemeProp = "light" | "dark";
 
@@ -19,7 +24,7 @@ const baseTheme = EditorView.baseTheme({
 });
 
 interface IEditorProps extends ReactCodeMirrorProps {
-  session: any;
+  session: Session;
   target: string;
   id: string;
 }
@@ -30,29 +35,28 @@ function Editor({ session, target, id, ...props }: IEditorProps) {
 
   const themeName = useMemo(() => codeMirrorTheme(theme), [theme]);
 
-  // useEffect(() => {
-  //   (async () => {
-  //     if (!session || !id || !target) return [];
-  //     const cm = await import("@flok/codemirror");
-  //     const setup = cm.flokBasicSetup(session, id, target);
-  //     console.log("setup", setup);
-  //     setFlokSetup(setup);
-  //   })();
-  // }, [session, id, target]);
-
   // useEffect only runs on the client, so now we can safely show the UI
   useEffect(() => {
     setMounted(true);
   }, []);
 
-  if (!mounted) {
+  if (!mounted || !session) {
     return null;
   }
+
+  const text = session.getText(id);
+  const undoManager = new UndoManager(text);
 
   return (
     <CodeMirror
       theme={themeName}
-      extensions={[baseTheme, javascript()]}
+      extensions={[
+        baseTheme,
+        flashField(),
+        Prec.high(evalKeymap(session, id, target)),
+        yCollab(text, session.awareness, { undoManager }),
+        javascript(),
+      ]}
       basicSetup={{
         foldGutter: false,
         lineNumbers: false,
