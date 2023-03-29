3de62b027cc11a3b3447fa8a3ac45f4dd898e708
Damián Silvani <munshkr@gmail.com>
2023-03-28 21:11:33 -0300

    refactor: Remove App.tsx

--- packages/web/src/App.tsx
+++
@@ -1,9 +0,0 @@
-export default function App() {
-  return (
-    <div>
-      <h1 className="text-center scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        The Joke Tax Chronicles
-      </h1>
-    </div>
-  );
-}
