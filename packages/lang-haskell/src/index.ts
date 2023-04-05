0aab5937305b9cf07f5e0a92aa3eece3ac0c9117
Damián Silvani <munshkr@gmail.com>
2023-04-05 18:38:19 -0300

    feat: Create lang-haskell (using template)

---
+++ packages/lang-haskell/src/index.ts
@@ -0,0 +1,37 @@
+import { parser } from "./syntax.grammar";
+import {
+  LRLanguage,
+  LanguageSupport,
+  indentNodeProp,
+  foldNodeProp,
+  foldInside,
+  delimitedIndent,
+} from "@codemirror/language";
+import { styleTags, tags as t } from "@lezer/highlight";
+
+export const haskellLanguage = LRLanguage.define({
+  parser: parser.configure({
+    props: [
+      indentNodeProp.add({
+        Application: delimitedIndent({ closing: ")", align: false }),
+      }),
+      foldNodeProp.add({
+        Application: foldInside,
+      }),
+      styleTags({
+        Identifier: t.variableName,
+        Boolean: t.bool,
+        String: t.string,
+        LineComment: t.lineComment,
+        "( )": t.paren,
+      }),
+    ],
+  }),
+  languageData: {
+    commentTokens: { line: ";" },
+  },
+});
+
+export function haskell() {
+  return new LanguageSupport(haskellLanguage);
+}
