fb60afc43abea74cde36f2654fb6dd1052761923
Damián Silvani <munshkr@gmail.com>
2023-04-05 18:42:08 -0300

    feat(web): Set different language extensions based on target

--- packages/web/src/components/editor.tsx
+++ packages/web/src/components/editor.tsx
@@ -2,11 +2,22 @@ import { useEffect, useState } from "react";
 import { EditorView } from "@codemirror/view";
 import CodeMirror, { ReactCodeMirrorProps } from "@uiw/react-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
+import { python } from "@codemirror/lang-python";
+import { haskell } from "@flok/lang-haskell";
 import { flashField, remoteEvalFlash, evalKeymap } from "@flok/cm-eval";
 import { yCollab } from "y-codemirror.next";
 import { UndoManager } from "yjs";
 import { Prec } from "@codemirror/state";
 import type { Document } from "@flok/session";
+import { langByTarget as languagesByTarget } from "@/settings.json";
+
+const defaultLanguage = "javascript";
+const langByTarget = languagesByTarget as { [lang: string]: string };
+const langExtensionsByLanguage: { [lang: string]: any } = {
+  javascript: javascript,
+  python: python,
+  haskell: haskell,
+};
 
 const baseTheme = EditorView.baseTheme({
   ".cm-scroller": { fontFamily: "Inconsolata", fontWeight: 600 },
@@ -42,11 +53,14 @@ function Editor({ document, ...props }: IEditorProps) {
     return null;
   }
 
+  const language: string = langByTarget[document.target] || defaultLanguage;
+  const languageExtension = langExtensionsByLanguage[language];
+
   return (
     <CodeMirror
       value={document.content}
       theme={themeName}
-      extensions={[baseTheme, flokSetup(document), javascript()]}
+      extensions={[baseTheme, flokSetup(document), languageExtension()]}
       basicSetup={{
         foldGutter: false,
         lineNumbers: false,
