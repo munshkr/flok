6bf93616dc95ed8535e9dc70707fedc3c2fe917d
Damián Silvani <munshkr@gmail.com>
2023-04-06 23:55:12 -0300

    chore(pubsub): Add node examples

---
+++ packages/pubsub/examples/client.js
@@ -0,0 +1,40 @@
+import { PubSubClient } from "./dist/index.js"
+
+const client = new PubSubClient({ url: "ws://localhost:4000" });
+
+// Subscribe to topics
+client.subscribe("b");
+client.subscribe("a");
+
+client.start();
+
+// Wait for the connection to be made, otherwise published messages are discarded.
+let internalId;
+client.on("open", () => {
+  // Publish a message (any JSON serializable object) to a topic
+  internalId = setInterval(() => {
+    client.publish("a", { salutation: "hello!" })
+  }, 2000);
+})
+
+// Add an error event handler to ignore connection errors
+client.on("error", () => { });
+
+client.on("close", () => clearInterval(internalId))
+
+setTimeout(() => {
+  // Unsubscribe from a topic
+  client.unsubscribe("b");
+  // ... or from all topics with a single call
+  client.unsubscribeAll();
+}, 5000)
+
+// you can add a listener for all message events
+client.on("message", (topic, data) => {
+  console.log("message", topic, data)
+})
+
+// ...or only from a specific topic
+client.on("message:a", (data) => {
+  console.log("message from topic 'a'", data)
+})
---
+++ packages/pubsub/examples/server.js
@@ -0,0 +1,7 @@
+import { WebSocketServer } from "ws"
+import { PubSubServer } from "./dist/server.js"
+
+const wss = new WebSocketServer({ port: 4000 });
+const server = new PubSubServer({ wss });
+
+console.log(`PubSub server listening on`, wss.address())
