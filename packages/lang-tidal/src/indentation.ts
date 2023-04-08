5e6c57e9fa1ef74ee5989f24d2db475c0ec6566b
Damián Silvani <munshkr@gmail.com>
2023-04-08 12:44:30 -0300

    fix(lang-tidal): Remove indentation when line is empty

--- packages/lang-tidal/src/indentation.ts
+++ packages/lang-tidal/src/indentation.ts
@@ -2,17 +2,13 @@ import { indentService } from "@codemirror/language";
 
 export function indentation() {
   return indentService.of((context, pos) => {
-    if (context.lineIndent(pos, -1)) {
-      return;
-    }
-
     let { text, from } = context.lineAt(pos, -1);
     let parse = text.slice(0, pos - from).match(/^([^$#]+)\$.*/);
 
     if (parse) {
       return Math.min(parse[1].length, 8);
     } else {
-      return null;
+      return 0;
     }
   });
 }
