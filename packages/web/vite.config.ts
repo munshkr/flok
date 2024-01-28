50d34b7b06607a1c739b59ad3d1b6cd05ff67df3
Damián Silvani <munshkr@gmail.com>
2024-01-28 17:31:34 -0300

    fix(web): Update tsconfig settings file based on Vite 5 template project

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,4 +1,4 @@
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
 import Unfonts from "unplugin-fonts/vite";
 import { defineConfig } from "vite";
