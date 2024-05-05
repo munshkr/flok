0e1d5901e35dc88c40ccf056473d23a6829867a3
Raphaël Forment <raphael.forment@gmail.com>
2024-05-05 22:39:47 +0200

    each theme its own file

---
+++ packages/web/src/lib/themes/index.ts
@@ -0,0 +1,13 @@
+import { ayuDark } from "./ayu-dark";
+import { dracula } from "./dracula";
+import { gruvboxDark } from "./gruvbox-dark";
+import { monokai } from "./monokai-dimmed";
+import { tokyoNight } from "./tokyo-night";
+
+export const codeMirrorThemes = {
+    "dracula": dracula,
+    "monokai": monokai,
+    "gruvbox": gruvboxDark,
+    "ayuDark": ayuDark,
+    "tokyonight": tokyoNight,
+};
