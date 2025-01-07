65ed5edf76f7fbb163a3646ac28402c9860f0ebd
taconi <igor.taconi@protonmail.com>
2025-01-07 18:08:13 -0300

    Adding py5 copyright

--- packages/web/src/lib/py5-wrapper.ts
+++ packages/web/src/lib/py5-wrapper.ts
@@ -1,4 +1,24 @@
 const wrapperContent = `
+# *****************************************************************************
+#
+#   Part of the py5 library
+#   Copyright (C) 2020-2024 Jim Schmitz
+#
+#   This library is free software: you can redistribute it and/or modify it
+#   under the terms of the GNU Lesser General Public License as published by
+#   the Free Software Foundation, either version 2.1 of the License, or (at
+#   your option) any later version.
+#
+#   This library is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+#   General Public License for more details.
+#
+#   You should have received a copy of the GNU Lesser General Public License
+#   along with this library. If not, see <https://www.gnu.org/licenses/>.
+#
+# *****************************************************************************
+
 # This pyp5js version is adapted to be more similar to py5 (py5coding.org)
 # by Alexandre B A Villares - https://abav.lugaralgum.com
 
